13331bf13b2ef524aa8511424cd1fb994512e2a4
KingArturSch <runks@ya.ru>
2017-11-22 12:26:25 +0300

    First commit

--- 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -6,25 +6,21 @@ package com.javarush.task.task16.task1617;
 
 public class Solution {
     public static volatile int countSeconds = 4;
-
     public static void main(String[] args) throws InterruptedException {
         RacingClock clock = new RacingClock();
-        //add your code here - добавь код тут
         Thread.sleep(3500);
         clock.interrupt();
     }
-
     public static class RacingClock extends Thread {
         public RacingClock() {
             start();
         }
 
         public void run() {
-            //add your code here - добавь код тут
             try {
                 while (countSeconds != 0) {
-                System.out.print(countSeconds + " ");
-                countSeconds--;
+                    System.out.print(countSeconds + " ");
+                    countSeconds--;
                     Thread.sleep(1000);
                 }
                 System.out.println("Марш!");
@@ -32,9 +28,6 @@ public class Solution {
                 System.out.println("Прервано!");
                 e.printStackTrace();
             }
-
-
-
         }
     }
 }
